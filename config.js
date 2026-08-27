59311fe1c6eaa3dba6f2605e4ab0a0c356595298
tommylee1208com-666 <tommylee1208.com@gmail.com>
2026-08-27 13:52:29 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// Reuse this template by changing only these two URLs.
+window.SITE_CONFIG = {
+  imageUrl: 'https://github.com/tommylee1208com-666/TK-Surprising-Posture/blob/main/main%E4%B8%87%E5%9C%A3%E8%8A%82111.png?raw=true',
+  shopUrl: 'https://shop.tiktok.com/us/store/wasuphomey/7494662854441207476'
+};
